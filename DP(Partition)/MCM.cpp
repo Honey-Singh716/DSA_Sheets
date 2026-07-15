17d0662324f248d16cb779214b14f3a6115455c8
Honey Singh <honeysingh.work12@gmail.com>
2026-07-15 18:56:03 +0530

    Added tabulation in MCM

--- DP(Partition)/MCM.cpp
+++ DP(Partition)/MCM.cpp
@@ -20,10 +20,34 @@ int MCM(int i,int j,vector<int>& nums,vector<vector<int>>& dp){
 
 int main(){
     vector<int> nums = {1,2,3,4,5};
+ 
+    int n = nums.size();
 
     vector<vector<int>> dp(nums.size(),vector<int>(nums.size(),-1));
 
-    cout<<MCM(1,nums.size()-1,nums,dp);
+    cout<<MCM(1,nums.size()-1,nums,dp)<<endl;
+    
+    int dps[n][n];
+
+    for(int i=0;i<n;i++){
+        dps[i][i] = 0;
+    }
+
+    
+    for(int i=n-1;i>=1;i--){
+        for(int j= i+1;j<n;j++){
+            int mini = 1e9;
+            for(int k = i;k<j;k++){
+                int steps = nums[i-1]*nums[k]*nums[j] + dps[i][k] + dps[k+1][j];
+
+                mini= min(mini ,steps);
+            }
+            dps[i][j] = mini;
+        }
+    }
+
+
+    cout<<dps[1][n-1];
 
     return 0;
 }
